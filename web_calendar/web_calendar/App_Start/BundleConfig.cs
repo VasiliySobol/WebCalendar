ed0a106cf30eac61ee802033347b7f3a49a74752
untilovdmitriy <untilovdmitriy@gmail.com>
2015-10-18 19:52:32 +0300

    view changes
    View changes includes:
    Index.cshtml
    - adding mini calendar(bootstrap.datepicker package)
    - adding navigation buttons
    Layout.cshtml
    - edit of navigation bar
    Site.css
    -adding custom css for calendar view

--- web_calendar/web_calendar/App_Start/BundleConfig.cs
+++ web_calendar/web_calendar/App_Start/BundleConfig.cs
@@ -7,7 +7,7 @@ namespace web_calendar
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
-        {
+        {          
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -20,12 +20,28 @@ namespace web_calendar
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+
+                                 "~/Scripts/bootstrap.js",
+
+                                 "~/Scripts/respond.js",
+
+                                 "~/Scripts/bootstrap-datepicker.js",
+
+                                 "~/Scripts/DatePickerReady.js"
+
+                                 ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+
+                                 "~/Content/bootstrap.css",
+
+                                 "~/Content/bootstrap-datepicker.css",
+
+                                 "~/Content/bootstrap-datepicker3.css",
+
+                                 "~/Content/site.css"
+
+                                 ));
         }
     }
 }
---
+++ web_calendar/web_calendar/Scripts/miniCalendar.js
@@ -0,0 +1,49 @@
+﻿function miniCalendar(id, year, month) {
+    var Dlast = new Date(year, month + 1, 0).getDate(),
+        D = new Date(year, month, Dlast),
+        DNlast = D.getDay(),
+        DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay(),
+        calendar = '<tr>',
+        m = document.querySelector('#' + id + ' option[value="' + D.getMonth() + '"]'),
+        g = document.querySelector('#' + id + ' input');
+    if (DNfirst != 0) {
+        for (var i = 1; i < DNfirst; i++) calendar += '<td>';
+    } else {
+        for (var i = 0; i < 6; i++) calendar += '<td>';
+    }
+    for (var i = 1; i <= Dlast; i++) {
+        if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+            calendar += '<td class="today">' + i;
+        } else {
+            if (  // список официальных праздников
+                (i == 1 && D.getMonth() == 0 && ((D.getFullYear() > 1897 && D.getFullYear() < 1930) || D.getFullYear() > 1947)) || // Новый год
+                (i == 2 && D.getMonth() == 0 && D.getFullYear() > 1992) || // Новый год
+                ((i == 3 || i == 4 || i == 5 || i == 6 || i == 8) && D.getMonth() == 0 && D.getFullYear() > 2004) || // Новый год
+                (i == 7 && D.getMonth() == 0 && D.getFullYear() > 1990) || // Рождество Христово
+                (i == 23 && D.getMonth() == 1 && D.getFullYear() > 2001) || // День защитника Отечества
+                (i == 8 && D.getMonth() == 2 && D.getFullYear() > 1965) || // Международный женский день
+                (i == 1 && D.getMonth() == 4 && D.getFullYear() > 1917) || // Праздник Весны и Труда
+                (i == 9 && D.getMonth() == 4 && D.getFullYear() > 1964) || // День Победы
+
+                (i == 7 && D.getMonth() == 10 && (D.getFullYear() > 1926 && D.getFullYear() < 2005)) || // Октябрьская революция 1917 года
+                (i == 8 && D.getMonth() == 10 && (D.getFullYear() > 1926 && D.getFullYear() < 1992)) || // Октябрьская революция 1917 года
+                (i == 4 && D.getMonth() == 10 && D.getFullYear() > 2004) // День народного единства, который заменил Октябрьскую революцию 1917 года
+               ) {
+                calendar += '<td class="holiday">' + i;
+            } else {
+                calendar += '<td>' + i;
+            }
+        }
+        if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
+            calendar += '<tr>';
+        }
+    }
+    for (var i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
+    document.querySelector('#' + id + ' tbody').innerHTML = calendar;
+    g.value = D.getFullYear();
+    m.selected = true;
+    if (document.querySelectorAll('#' + id + ' tbody tr').length < 6) {
+        document.querySelector('#' + id + ' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
+    }
+    document.querySelector('#' + id + ' option[value="' + new Date().getMonth() + '"]').style.color = 'rgb(220, 0, 0)'; // в выпадающем списке выделен текущий месяц
+}
