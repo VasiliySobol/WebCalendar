5289378f243c76fc4785dd36138beb886889829a
Amira <amira.sheremet@gmail.com>
2015-10-20 13:23:24 +0300

    added PasswordHasher in new project Infrastructure

---
+++ web_calendar/Infrastructure/Tools/PasswordHasher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Tools
+{
+    public static class PasswordHasher
+    {
+        public static string Encript(string password)
+        {
+            MD5 md5Hash = MD5.Create();
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
