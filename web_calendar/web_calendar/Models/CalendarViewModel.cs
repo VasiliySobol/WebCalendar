499f5feca4a5f8d3460a633ccde354329a5172e7
Amira <amira.sheremet@gmail.com>
2015-10-23 20:46:33 +0300

    renamed Calendar to CalendarViewModel in web_calendar/Models

--- web_calendar/web_calendar/Models/CalendarViewModel.cs
+++ web_calendar/web_calendar/Models/CalendarViewModel.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Drawing;
+using System.ComponentModel.DataAnnotations;
 
 namespace web_calendar.Models
 {
-    public class Calendar
+    public class CalendarViewModel
     {
+        [Required]
         public string Name { get; set; }
+
         public string Text { get; set; }
+
         public  Color color { get; set; }
-        //public List<Events> { get; set; }
+
         bool _visibility { get; set; }
+
         string _timezone {get; set; }
+
         int _userId {get; set; }
-        //NotificationType _notificationType { get; set; }
     }
 }
