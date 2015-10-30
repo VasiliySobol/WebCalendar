828ca467d1fdb101b6ece91593c469856a932293
Amira <amira.sheremet@gmail.com>
2015-10-30 15:05:59 +0200

    deleted temporary files

--- web_calendar/web_calendar/Models/AccountViewModels.cs
+++ web_calendar/web_calendar/Models/AccountViewModels.cs
@@ -65,6 +65,10 @@ namespace web_calendar.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
