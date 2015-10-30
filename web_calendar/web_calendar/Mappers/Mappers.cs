a72f319635eee79eb186b184f2b98c7522271f99
Amira <amira.sheremet@gmail.com>
2015-10-30 15:45:38 +0200

    added Mapper class

---
+++ web_calendar/web_calendar/Mappers/Mappers.cs
@@ -0,0 +1,51 @@
+﻿using ApplicationRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using web_calendar.Models;
+
+namespace web_calendar.Mappers
+{
+    public static class Mapper
+    {
+        // -------- Events --------
+        public static EventViewModel Map(Event _event, ICollection<NotificationType> notificationType,
+            ICollection<Repeatable> repeatable)
+        {
+            EventViewModel eventVM = new EventViewModel();
+
+            throw new NotImplementedException();
+
+            return eventVM;
+        }
+
+        public static Event MapToEvent(EventViewModel eventVM)
+        {
+            Event _event = new Event();
+
+            _event.name = eventVM.Name;
+            _event.text = eventVM.Text;
+            _event.place = eventVM.Place;
+            _event.time_begin = eventVM.TimeBegin;
+            _event.time_end = eventVM.TimeEnd;
+            _event.visibility = eventVM.Visibility;
+            _event.all_day = eventVM.AllDay;
+
+            return _event;
+        }
+
+        public static NotificationType MapToNotificationType(EventViewModel eventVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Repeatable MapToRepeatable(EventViewModel eventVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        // -------- Calendars --------
+        // -------- Users --------
+    }
+}
