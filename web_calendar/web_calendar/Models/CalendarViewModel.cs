2ec8b78a5ff19e59b85784c177752cce6555de83
Amira <amira.sheremet@gmail.com>
2015-10-30 15:00:26 +0200

    added fiels for notification settings in CalendarViewModel

--- web_calendar/web_calendar/Models/CalendarViewModel.cs
+++ web_calendar/web_calendar/Models/CalendarViewModel.cs
@@ -11,14 +11,14 @@ namespace web_calendar.Models
     {
         [Required]
         public string Name { get; set; }
-
         public string Text { get; set; }
-
-        public  Color color { get; set; }
-
-        bool _visibility { get; set; }
-
-        string _timezone {get; set; }
+        public  Color Color { get; set; }
+        bool Visibility { get; set; }
+        string TimeZone { get; set; }
+        public Nullable<int> RepetitionCount { get; set; }
+        public Nullable<int> Interval { get; set; }
+        public Nullable<int> TimeBefore { get; set; }
+        public string KindOfNotification { get; set; }
 
         int _userId {get; set; }
     }
