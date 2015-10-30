a26164701d3ea9d9f25cc5952051dfda35c00508
Amira <amira.sheremet@gmail.com>
2015-10-30 15:46:14 +0200

    added annotation in CalendarViewModel

--- web_calendar/web_calendar/Models/CalendarViewModel.cs
+++ web_calendar/web_calendar/Models/CalendarViewModel.cs
@@ -9,15 +9,33 @@ namespace web_calendar.Models
 {
     public class CalendarViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Name is required.")]
+        [Display(Name = "Name")]
         public string Name { get; set; }
+
+        [Display(Name = "Name")]
         public string Text { get; set; }
-        public  Color Color { get; set; }
-        bool Visibility { get; set; }
-        string TimeZone { get; set; }
+
+        [Display(Name = "Color")]
+        public Color Color { get; set; }
+
+        [Display(Name = "Visibility")]
+        public string Visibility { get; set; }
+
+        [Display(Name = "Time zone")]
+        public string TimeZone { get; set; }
+
+        // Notification Settings
+        [Display(Name = "Repetition count")]
         public Nullable<int> RepetitionCount { get; set; }
+
+        [Display(Name = "Interval")]
         public Nullable<int> Interval { get; set; }
+
+        [Display(Name = "Time before event")]
         public Nullable<int> TimeBefore { get; set; }
+
+        [Display(Name = "Kind of notification")]
         public string KindOfNotification { get; set; }
 
         int _userId {get; set; }
