0d7f662fe5e09145ce11257841d2c163d9269c94
Amira <amira.sheremet@gmail.com>
2015-10-31 00:58:06 +0200

    updated entites' names

--- web_calendar/web_calendar/Mappers/Mappers.cs
+++ web_calendar/web_calendar/Mappers/Mappers.cs
@@ -10,18 +10,19 @@ namespace web_calendar.Mappers
     public static class Mapper
     {
         // -------- Events --------
-        public static EventViewModel Map(Event _event, ICollection<NotificationType> notificationType,
+        public static EventViewModel Map(CalendarEvent calendarEvent, 
+            ICollection<NotificationType> notificationType,
             ICollection<Repeatable> repeatable, List<string> emails)
         {
             EventViewModel eventVM = new EventViewModel();
 
-            eventVM.Id = _event.id;
-            eventVM.Name = _event.name;
-            eventVM.Text = _event.text;
-            eventVM.Place = _event.place;
-            eventVM.Visibility = _event.visibility;
-            eventVM.TimeBegin = _event.time_begin;
-            eventVM.AllDay = _event.all_day;
+            eventVM.Id = calendarEvent.Id;
+            eventVM.Name = calendarEvent.Name;
+            eventVM.Text = calendarEvent.Text;
+            eventVM.Place = calendarEvent.Place;
+            eventVM.Visibility = calendarEvent.Visibility;
+            eventVM.TimeBegin = calendarEvent.TimeBegin;
+            eventVM.AllDay = calendarEvent.AllDay;
 
             // Notification Settings
             List<NotificationSettingsViewModel> list = new List<NotificationSettingsViewModel>();
@@ -44,39 +45,40 @@ namespace web_calendar.Mappers
             return eventVM;
         }
 
-        public static NotificationSettingsViewModel MapToNotificationSettingsViewModel(NotificationType notificationType)
+        public static NotificationSettingsViewModel MapToNotificationSettingsViewModel(
+            NotificationType notificationType)
         {
             NotificationSettingsViewModel notificationSettingsVM = new NotificationSettingsViewModel();
-            notificationSettingsVM.Id = notificationType.id;
-            notificationSettingsVM.Interval = notificationType.interval;
-            notificationSettingsVM.KindOfNotification = notificationType.kind_of_notification;
-            notificationSettingsVM.RepetitionCount = notificationType.repetition_count;
-            notificationSettingsVM.TimeBefore = notificationType.time_before;
+            notificationSettingsVM.Id = notificationType.Id;
+            notificationSettingsVM.Interval = notificationType.Interval;
+            notificationSettingsVM.KindOfNotification = notificationType.KindOfNotification;
+            notificationSettingsVM.RepetitionCount = notificationType.RepetitionCount;
+            notificationSettingsVM.TimeBefore = notificationType.TimeBefore;
             return notificationSettingsVM;
         }
 
         public static RepeatableSettingsViewModel MapToRepeatableSettingsViewModel(Repeatable repeatable)
         {
             RepeatableSettingsViewModel repeatableSettingsVM = new RepeatableSettingsViewModel();
-            repeatableSettingsVM.Id = repeatable.id;
-            repeatableSettingsVM.Period = repeatable.period;
-            repeatableSettingsVM.RepeatCount = repeatable.repeat_count;
+            repeatableSettingsVM.Id = repeatable.Id;
+            repeatableSettingsVM.Period = repeatable.Period;
+            repeatableSettingsVM.RepeatCount = repeatable.RepeatCount;
             return repeatableSettingsVM;
         }
 
-        public static Event MapToEvent(EventViewModel eventVM)
+        public static CalendarEvent MapToEvent(EventViewModel eventVM)
         {
-            Event _event = new Event();
+            CalendarEvent calendarEvent = new CalendarEvent();
 
-            _event.name = eventVM.Name;
-            _event.text = eventVM.Text;
-            _event.place = eventVM.Place;
-            _event.time_begin = eventVM.TimeBegin;
-            _event.time_end = eventVM.TimeEnd;
-            _event.visibility = eventVM.Visibility;
-            _event.all_day = eventVM.AllDay;
+            calendarEvent.Name = eventVM.Name;
+            calendarEvent.Text = eventVM.Text;
+            calendarEvent.Place = eventVM.Place;
+            calendarEvent.TimeBegin = eventVM.TimeBegin;
+            calendarEvent.TimeEnd = eventVM.TimeEnd;
+            calendarEvent.Visibility = eventVM.Visibility;
+            calendarEvent.AllDay = eventVM.AllDay;
 
-            return _event;
+            return calendarEvent;
         }
 
         public static ICollection<NotificationType> MapToNotificationTypes(EventViewModel eventVM)
@@ -92,10 +94,10 @@ namespace web_calendar.Mappers
         public static NotificationType MapToNotificationType(NotificationSettingsViewModel notificationSettingsVM)
         {
             NotificationType notificationType = new NotificationType();
-            notificationType.interval = notificationSettingsVM.Interval;
-            notificationType.kind_of_notification = notificationSettingsVM.KindOfNotification;
-            notificationType.repetition_count = notificationSettingsVM.RepetitionCount;
-            notificationType.time_before = notificationSettingsVM.TimeBefore;
+            notificationType.Interval = notificationSettingsVM.Interval;
+            notificationType.KindOfNotification = notificationSettingsVM.KindOfNotification;
+            notificationType.RepetitionCount = notificationSettingsVM.RepetitionCount;
+            notificationType.TimeBefore = notificationSettingsVM.TimeBefore;
             return notificationType;
         }
 
@@ -112,10 +114,10 @@ namespace web_calendar.Mappers
         public static Repeatable MapToRepeatable(RepeatableSettingsViewModel repeatableSettingsVM)
         {
             Repeatable repeatable = new Repeatable();
-            repeatable.period = repeatableSettingsVM.Period;
+            repeatable.Period = repeatableSettingsVM.Period;
             if (repeatableSettingsVM.RepeatCount != null)
             {
-                repeatable.repeat_count = repeatableSettingsVM.RepeatCount;
+                repeatable.RepeatCount = repeatableSettingsVM.RepeatCount;
             }
             else
             {
