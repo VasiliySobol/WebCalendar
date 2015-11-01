5eed9f6e78d46207513e8121ade915c2ca1a6d11
untilovdmitriy <untilovdmitriy@gmail.com>
2015-11-01 20:31:42 +0200

    change type of fields in order of Model in DB

--- web_calendar/web_calendar/Models/CalendarViewModel.cs
+++ web_calendar/web_calendar/Models/CalendarViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Drawing;
 using System.ComponentModel.DataAnnotations;
 
 namespace web_calendar.Models
@@ -13,17 +12,17 @@ namespace web_calendar.Models
         [Display(Name = "Name")]
         public string Name { get; set; }
 
-        [Display(Name = "Name")]
-        public string Text { get; set; }
+        [Display(Name = "Description")]
+        public string Description { get; set; }
 
         [Display(Name = "Color")]
-        public Color CalendarColor { get; set; }
+        public int? CalendarColor { get; set; }
 
         [Display(Name = "Visibility")]
         public string Visibility { get; set; }
 
         [Display(Name = "Time zone")]
-        public string TimeZone { get; set; }
+        public byte? TimeZone { get; set; }
 
         // Notification Settings
         public NotificationSettingsViewModel notificationSettings { get; set; }        
