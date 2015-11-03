7dcbe18bf0bdd02adc369ce5d99f855657db2aa1
Amira <amira.sheremet@gmail.com>
2015-11-03 22:09:28 +0200

    disable test template

--- web_calendar/web_calendar.Tests/Controllers/HomeControllerTest.cs
+++ web_calendar/web_calendar.Tests/Controllers/HomeControllerTest.cs
@@ -12,43 +12,44 @@ namespace web_calendar.Tests.Controllers
     [TestClass]
     public class HomeControllerTest
     {
-        [TestMethod]
-        public void Index()
-        {
-            // Arrange
-            CalendarController controller = new CalendarController();
+        //    [TestMethod]
+        //    public void Index()
+        //    {
+        //        // Arrange
+        //        CalendarController controller = new CalendarController();
 
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
+        //        // Act
+        //        ViewResult result = controller.Index() as ViewResult;
 
-            // Assert
-            Assert.IsNotNull(result);
-        }
+        //        // Assert
+        //        Assert.IsNotNull(result);
+        //    }
 
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            CalendarController controller = new CalendarController();
+        //    [TestMethod]
+        //    public void About()
+        //    {
+        //        // Arrange
+        //        CalendarController controller = new CalendarController();
 
-            // Act
-            ViewResult result = controller.About() as ViewResult;
+        //        // Act
+        //        ViewResult result = controller.About() as ViewResult;
 
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
+        //        // Assert
+        //        Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        //    }
 
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            CalendarController controller = new CalendarController();
+        //    [TestMethod]
+        //    public void Contact()
+        //    {
+        //        // Arrange
+        //        CalendarController controller = new CalendarController();
 
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
+        //        // Act
+        //        ViewResult result = controller.Contact() as ViewResult;
 
-            // Assert
-            Assert.IsNotNull(result);
-        }
+        //        // Assert
+        //        Assert.IsNotNull(result);
+        //    }
+        //}
     }
 }
