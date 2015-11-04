76afe5a6156e27c3e43d282a38ad727cd902b96c
Amira <amira.sheremet@gmail.com>
2015-11-04 23:58:52 +0200

    now using unity container

--- web_calendar/web_calendar/Controllers/EventController.cs
+++ web_calendar/web_calendar/Controllers/EventController.cs
@@ -6,21 +6,33 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using ApplicationRepository.Models;
 using web_calendar.Models;
 using web_calendar.Mappers;
+using ApplicationRepository.Interface;
 
 namespace web_calendar.Controllers
 {
+    [Authorize]
     public class EventController : Controller
     {
-        private web_calendarEntities db = new web_calendarEntities();
+        public IEventRepository eventRepository;
+        public INotificationRepository notificationRepository;
 
+        public EventController(IEventRepository _eventRepository, INotificationRepository _notificationRepository)  
+        {
+            this.eventRepository = _eventRepository;
+            this.notificationRepository = _notificationRepository;
+        }
         // GET: Event/Schedule
         public ActionResult Schedule()
         {
+            string userId = User.Identity.GetUserId();
             List<DisplayEventViewModel> list = new List<DisplayEventViewModel>();
-            foreach (CalendarEvent item in db.CalendarEvents.ToList())
+            foreach (CalendarEvent item in eventRepository.GetAllUserEvents(userId))
             {
                 list.Add(Mapper.MapToDisplayEventVM(item));
             }
@@ -28,22 +40,16 @@ namespace web_calendar.Controllers
         }
         
         // GET: Event/Details/5
-        public ActionResult Details(int? id)
+        public ActionResult Details(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CalendarEvent calendarEvent = db.CalendarEvents.Find(id);
+            CalendarEvent calendarEvent = eventRepository.FindById(id);
             if (calendarEvent == null)
             {
                 return HttpNotFound();
             }
-            ////TODO
-            //ICollection<NotificationType> notificationTypes;
-            //ICollection<Repeatable> repeatables;
-            //return View(Mapper.Map(_event, notificationTypes, repeatables));
-            return View(calendarEvent);
+            List<NotificationType> notifications = notificationRepository.GetAllEventNotificationTypes(id);
+            List<Repeatable> repeatables = calendarEvent.Repeatables.ToList();
+            return View(Mapper.MapToDetailsEventVM(calendarEvent, notifications, repeatables));
         }
 
         // GET: Event/Create
@@ -53,71 +59,59 @@ namespace web_calendar.Controllers
         }
 
         // POST: Event/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Text,Place,TimeBegin,TimeEnd,Visibility,AllDay,RepetitionCount,Interval,TimeBefore,KindOfNotification,IfRepeatable,Period,RepeatCount,EndDate,CalendarId,CalendarName")] CreateEventViewModel eventViewModel)
+        public ActionResult Create([Bind(Include = "Name,Text,Place,TimeBegin,TimeEnd,Visibility,AllDay,RepetitionCount,Interval,TimeBefore,KindOfNotification,IfRepeatable,Period,RepeatCount,EndDate,CalendarName")] CreateEventViewModel eventViewModel)
         {
             if (ModelState.IsValid)
             {
                 CalendarEvent calendarEvent = Mapper.MapToEvent(eventViewModel);
                 //TODO add logic
-                db.CalendarEvents.Add(calendarEvent);
-                db.SaveChanges();
+                eventRepository.Add(calendarEvent);
+                eventRepository.SaveChanges();
                 return RedirectToAction("Index");
             }
-
             return View(eventViewModel);
         }
 
         // GET: Event/Edit/5
-        public ActionResult Edit(int? id)
+        public ActionResult Edit(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CalendarEvent calendarEvent = db.CalendarEvents.Find(id);
+            CalendarEvent calendarEvent = eventRepository.FindById(id);
             if (calendarEvent == null)
             {
                 return HttpNotFound();
             }
             //TODO add logic
-            CreateEventViewModel eventViewModel = new CreateEventViewModel();
+            CreateEventViewModel eventViewModel = Mapper.MapToEditEventVM(calendarEvent);
             return View(eventViewModel);
         }
 
         // POST: Event/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Text,Place,TimeBegin,TimeEnd,Visibility,AllDay,RepetitionCount,Interval,TimeBefore,KindOfNotification,IfRepeatable,Period,RepeatCount,EndDate,CalendarId,CalendarName")] CreateEventViewModel eventViewModel)
+        public ActionResult Edit([Bind(Include = "Name,Text,Place,TimeBegin,TimeEnd,Visibility,AllDay,RepetitionCount,Interval,TimeBefore,KindOfNotification,IfRepeatable,Period,RepeatCount,EndDate,CalendarName")] CreateEventViewModel eventViewModel)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(eventViewModel).State = EntityState.Modified;
-                db.SaveChanges();
+                CalendarEvent calendarEvent = Mapper.MapToEvent(eventViewModel);
+                //TODO: add logic
+                eventRepository.Modify(calendarEvent);
+                eventRepository.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(eventViewModel);
         }
 
         // GET: Event/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CalendarEvent calendarEvent = db.CalendarEvents.Find(id);
+            CalendarEvent calendarEvent = eventRepository.FindById(id);
             if (calendarEvent == null)
             {
                 return HttpNotFound();
             }
-            //TODO add logic
-            CreateEventViewModel eventViewModel = new CreateEventViewModel();
+            DisplayEventViewModel eventViewModel = Mapper.MapToDisplayEventVM(calendarEvent);
             return View(eventViewModel);
         }
 
@@ -126,19 +120,10 @@ namespace web_calendar.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            CalendarEvent calendarEvent = db.CalendarEvents.Find(id);
-            db.CalendarEvents.Remove(calendarEvent);
-            db.SaveChanges();
+            CalendarEvent calendarEvent = eventRepository.FindById(id);
+            eventRepository.Delete(calendarEvent);
+            eventRepository.SaveChanges();
             return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
