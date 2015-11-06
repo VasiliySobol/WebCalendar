c0bf4f385b58de7bac1058fef3b7958e2f093663
SobolVasiliy <sobol.vasiliy@gmail.com>
2015-11-06 17:46:09 +0200

    Rename Infrastructure-project to web_calendar.BL-project

--- web_calendar/Infrastructure/Tools/NotificationTypes.cs
+++ web_calendar/Infrastructure/Tools/NotificationTypes.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Infrastructure.Tools
+namespace web_calendar.BL.Tools
 {
     public class NotificationTypes
     {
--- web_calendar/Infrastructure/Tools/PasswordHasher.cs
+++ web_calendar/Infrastructure/Tools/PasswordHasher.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 
-namespace Infrastructure.Tools
+namespace web_calendar.BL.Tools
 {
     public static class PasswordHasher
     {
