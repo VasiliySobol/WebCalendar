3c9c490ff9e5a8e0d3904627070f349711f95132
Amira <amira.sheremet@gmail.com>
2015-11-06 22:39:58 +0200

    added repositories

--- web_calendar/Infrastructure/Services/EventService.cs
+++ web_calendar/Infrastructure/Services/EventService.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using web_calendar.DAL.Interface;
 
 namespace web_calendar.BL.Services
 {
-    class EventService
+    public class EventService
     {
+        
+        public IEventRepository eventRepository;
+        public INotificationRepository notificationRepository;
+        public ICalendarRepository calendarRepository;
+
+        public EventService(IEventRepository _eventRepository, INotificationRepository _notificationRepository,
+            ICalendarRepository _calendarRepository)  
+        {
+            this.eventRepository = _eventRepository;
+            this.notificationRepository = _notificationRepository;
+            this.calendarRepository = _calendarRepository;
+        }
+
+
     }
 }
