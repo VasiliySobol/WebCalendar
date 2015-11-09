396e89be0163f3ac9ec15ebffbbcecaaa45f3447
Amira <amira.sheremet@gmail.com>
2015-11-09 19:32:15 +0200

    added RepeatableRepository

---
+++ web_calendar/ApplicationRepository/Interface/IRepeatableRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using web_calendar.DAL.Models;
+
+namespace web_calendar.DAL.Interface
+{
+    public interface IRepeatableRepository : IGenericRepository<Repeatable>
+    {
+
+    }
+}
