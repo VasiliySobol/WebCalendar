2188bf8e8991bdaf052b5a9c166b3b05c8821eb0
Amira <amira.sheremet@gmail.com>
2015-11-12 23:19:11 +0200

    added route for Event/Schedule as default

--- web_calendar/web_calendar/App_Start/RouteConfig.cs
+++ web_calendar/web_calendar/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace web_calendar
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Schedule",
+                url: "Event/{action}/{id}",
+                defaults: new { controller = "Event", action = "Schedule", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Calendar", action = "Index", id = UrlParameter.Optional }
