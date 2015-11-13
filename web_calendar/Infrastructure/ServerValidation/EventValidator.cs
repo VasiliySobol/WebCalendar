a8e69a1799898a3c8087ff77dd659b5b11110ad9
Amira <amira.sheremet@gmail.com>
2015-11-13 13:40:39 +0200

    added EventValidator to validate event viewmodels and Visibility enum

---
+++ web_calendar/Infrastructure/ServerValidation/EventValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using web_calendar.BL.ViewModels;
+
+namespace web_calendar.BL.ServerValidation
+{
+    public static class EventValidator
+    {
+        public static bool Validate(CreateEventViewModel eventVM)
+        {
+            bool ifValid = (eventVM.Name != null);
+            if (!ifValid) return false;
+            ifValid &= (eventVM.TimeBegin != null);
+            if (!ifValid) return false;
+            ifValid &= (eventVM.TimeBegin.CompareTo(DateTime.Now) > 0);
+            if (eventVM.TimeEnd != null)
+                ifValid &= (eventVM.TimeBegin.CompareTo(eventVM.TimeBegin) > 0);
+            // add other validation (visibility, etc.)
+            return ifValid;
+        }
+    }
+}
