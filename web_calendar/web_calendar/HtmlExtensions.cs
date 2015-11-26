ef2f37c8e559642ae12b083c63cd70c6df54bfb8
SobolVasiliy <sobol.vasiliy@gmail.com>
2015-11-26 09:32:31 +0200

    Add HtmlExtensions-class

---
+++ web_calendar/web_calendar/HtmlExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace web_calendar
+{
+    public static class HtmlExtensions
+    {
+    }
+}
