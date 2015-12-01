affb375cfbb53ab2b4a445c1dc83a641cad8fe8c
untilovdmitriy <untilovdmitriy@gmail.com>
2015-12-01 16:32:21 +0200

    some changes in CalendarList

--- web_calendar/web_calendar/Scripts/customScripts.js
+++ web_calendar/web_calendar/Scripts/customScripts.js
@@ -1,35 +1,29 @@
 ﻿arrayOfCalendars = new Array();
 
-function GetCalendarId(id)
-{
+function GetCalendarId(id) {
     return arrayOfCalendars[id];
 }
 
-function GetCalendarColor(id)
-{
+function GetCalendarColor(id) {
     alert(arrayOfCalendars[id].CSSColor);
 }
 
-function Add(data)
-{
+function Add(data) {
     arrayOfCalendars.push(data);
 }
 
-function SetsColor(idName)
-{
+function SetsColor(idName) {
     document.getElementById(idName).style.backgroundColor = "#AA0000";
     alert("fdsa");
 }
 
-function SetHeadColor(color)
-{
+function SetHeadColor(color) {
     for (var i = 1; i <= 7; i++) {
         document.getElementById("day" + i).style.backgroundColor = color;
     }
 }
 
-function SetMainColor(color, calendarLength)
-{
+function SetMainColor(color, calendarLength) {
     for (var i = 1; i < calendarLength; i++) {
         document.getElementById("cellId" + i).style.backgroundColor = color;
     }
