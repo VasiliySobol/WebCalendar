c6040a7803f9d44d3db2fecfc99bc3a05c530a59
SobolVasiliy <sobol.vasiliy@gmail.com>
2015-12-02 20:17:51 +0200

    Removing of isActive-field and adding calendarDateTime-field

--- web_calendar/Infrastructure/ViewModels/CalendarViewModel.cs
+++ web_calendar/Infrastructure/ViewModels/CalendarViewModel.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Drawing;
 using System.Web.Mvc;
+using web_calendar.BL.CustomClasses;
 
 namespace web_calendar.BL.ViewModels
 {
@@ -35,7 +36,7 @@ namespace web_calendar.BL.ViewModels
 
         public string CSSHeadColor { get; set; }
 
-        public bool isActive { get; set; }
+        public CalendarDateTime chronometer { get; set; }
 
         [HiddenInput(DisplayValue = false)]
         public string userId { get; set; }
