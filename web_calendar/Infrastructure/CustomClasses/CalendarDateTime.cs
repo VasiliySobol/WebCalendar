4191ee6a06860978d9184fa3f62e0418406112a0
SobolVasiliy <sobol.vasiliy@gmail.com>
2015-12-02 20:16:10 +0200

    Add CalendarDateTime-class

---
+++ web_calendar/Infrastructure/CustomClasses/CalendarDateTime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_calendar.BL.CustomClasses
+{
+    class CalendarDateTime
+    {
+        DateTime dateTime { get; set; }
+
+
+        public CalendarDateTime()
+        {
+            dateTime = DateTime.Now;
+        }
+    }
+}
