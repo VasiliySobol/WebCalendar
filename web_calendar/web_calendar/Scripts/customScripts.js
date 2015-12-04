cbbb82e6656d28da4c1cd2c3755b65655319f2ad
SobolVasiliy <sobol.vasiliy@gmail.com>
2015-12-04 12:55:31 +0200

    Removing unnecessary JS-scripts from customScripts.js-file and some changes at SetMainColor-script

--- web_calendar/web_calendar/Scripts/customScripts.js
+++ web_calendar/web_calendar/Scripts/customScripts.js
@@ -1,29 +1,11 @@
-﻿arrayOfCalendars = new Array();
-
-function GetCalendarId(id) {
-    return arrayOfCalendars[id];
-}
-
-function GetCalendarColor(id) {
-    alert(arrayOfCalendars[id].CSSColor);
-}
-
-function Add(data) {
-    arrayOfCalendars.push(data);
-}
-
-function kkk() {
-    alert("fdsa");
-}
-
-function SetHeadColor(color) {
+﻿function SetHeadColor(color) {
     for (var i = 1; i <= 7; i++) {
         document.getElementById("day" + i).style.backgroundColor = color;
     }
 }
 
-function SetMainColor(color, calendarLength) {
-    for (var i = 1; i < calendarLength; i++) {
+function SetMainColor(color) {
+    for (var i = 1; i <= 31; i++) {
         document.getElementById("cellId" + i).style.backgroundColor = color;
     }
 }
