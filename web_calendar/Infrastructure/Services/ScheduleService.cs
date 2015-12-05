ea132214203e9c02c14d3b52f3d1512bad0d413c
Amira <amira.sheremet@gmail.com>
2015-12-05 13:29:54 +0200

    added ScheduleService and NotificatioJob

---
+++ web_calendar/Infrastructure/Services/ScheduleService.cs
@@ -0,0 +1,48 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using web_calendar.DAL.Models;
+
+namespace web_calendar.BL.Services
+{
+    public static class ScheduleService
+    {
+        private static IScheduler schedule;
+        private static ISchedulerFactory scheduleFactory;
+
+        static ScheduleService()
+        {
+            // construct a scheduler factory
+            scheduleFactory = new StdSchedulerFactory();
+
+            // get a scheduler
+            schedule = scheduleFactory.GetScheduler();
+            schedule.Start();
+        }
+
+        public static void AddJob(string eventName, Notification notification, int minutes)
+        {
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<NotificationJob>()
+                .WithIdentity("myJob", "group1")
+                .UsingJobData("eventName", eventName)
+                .UsingJobData("kindOfNotification", notification.KindOfNotification)
+                .Build();
+
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+              .WithIdentity("myTrigger", "group1")
+              .StartNow()
+              .WithSimpleSchedule(x => x
+                  .WithRepeatCount(1)                  
+                  .WithIntervalInMinutes(minutes))
+              .Build();
+
+            schedule.ScheduleJob(job, trigger);
+        }
+    }
+}
