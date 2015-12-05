952c5c56fc12dff16a1dac0f621ae65e3fd0e52e
SobolVasiliy <sobol.vasiliy@gmail.com>
2015-12-05 19:08:12 +0200

    Small bugfix

--- web_calendar/Infrastructure/CustomClasses/CalendarDateTime.cs
+++ web_calendar/Infrastructure/CustomClasses/CalendarDateTime.cs
@@ -57,7 +57,7 @@ namespace web_calendar.BL.CustomClasses
             }
 
             string[] dateAndTime = _dateTime.Split(' ');
-            string[] dateData = dateAndTime[0].Split('.');
+            string[] dateData = dateAndTime[0].Split('.', '/');
             string[] timeData = dateAndTime[1].Split(':');
 
             this.dateTime = new DateTime(int.Parse(dateData[2]), int.Parse(dateData[1]), int.Parse(dateData[0]));
@@ -71,7 +71,7 @@ namespace web_calendar.BL.CustomClasses
             }
 
             string[] dateAndTime = _dateTime.Split(' ');
-            string[] dateData = dateAndTime[0].Split('.');
+            string[] dateData = dateAndTime[0].Split('.', '/');
             string[] timeData = dateAndTime[1].Split(':');
 
             this.dateTime = new DateTime(int.Parse(dateData[2]), int.Parse(dateData[1]) - 1, int.Parse(dateData[0]));
