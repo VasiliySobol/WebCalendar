bd51f1ff93acfb01865e09ad118fb047bf60ac59
Amira <amira.sheremet@gmail.com>
2015-12-05 20:49:24 +0200

    added methods to work with guests

--- web_calendar/ApplicationRepository/Interface/IEventRepository.cs
+++ web_calendar/ApplicationRepository/Interface/IEventRepository.cs
@@ -10,18 +10,24 @@ namespace web_calendar.DAL.Interface
     public interface IEventRepository: IGenericRepository<CalendarEvent>
     {
         IEnumerable<CalendarEvent> GetAllUserEvents(string userId);
+        void DeleteAllChildrenEvents(int parentId);
+
         Notification FindFirstNotification(int id, Func<Notification, bool> filter);
         IEnumerable<Notification> FindAllNotifications(int id, Func<Notification, bool> filter);
         IEnumerable<Notification> GetAllNotifications(int eventId);
         void AddNotifications(int eventId, IEnumerable<Notification> notifications);
         void DeleteNotifications(int eventId, IEnumerable<Notification> notifications);
+
         bool IsRepeatable(CalendarEvent calendarEvent);
         Repeatable GetRepeatableSettings(int id);
         void AddRepeatableSettings(int eventId, Repeatable repeatable);
+
         Calendar GetCalendar(int id);
         void AddCalendar(int eventId, int calendarId);
+
         void AddGuests(int eventId, List<string> emails);
+        void DeleteGuests(int eventId, List<string> emails);
         IEnumerable<Guest> FindAllGuests(int id, Func<Guest, bool> filter);
-        void DeleteAllChildrenEvents(int parentId);
+        IEnumerable<Guest> GetAllGuests(int id);
     }
 }
