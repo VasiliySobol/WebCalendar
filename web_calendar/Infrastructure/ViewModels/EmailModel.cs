72d589535f7c361237e09a905948f8911f044d28
Amira <amira.sheremet@gmail.com>
2015-12-05 13:30:16 +0200

    added EmailService and EmailModel

---
+++ web_calendar/Infrastructure/ViewModels/EmailModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_calendar.BL.ViewModels
+{
+    public class EmailModel
+    {
+        public string Name { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
