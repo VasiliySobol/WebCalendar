d0d48ca8ffa287ba98197f0f76f2aa87a96a524c
Amira <amira.sheremet@gmail.com>
2015-12-08 20:14:04 +0200

    created GuestInvitation class to invite guests

---
+++ web_calendar/Infrastructure/UserMails/GuestInvitation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using web_calendar.Infrastructure.Services;
+
+namespace web_calendar.BL.UserMails
+{
+    public static class GuestInvitation
+    {
+        public static void SendInvitations(List<string> guests, string userEmail, Invitation invitation)
+        {
+            foreach (string guest in guests)
+            {
+                EmailModel email = new EmailModel { To = guest, From = userEmail, Subject = "Invitation", 
+                    Body = invitation.GetInvitation() };
+                EmailSender.SendMail(email);
+            }
+        }
+    }
+}
