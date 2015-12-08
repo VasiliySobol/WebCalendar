7480cbaa42f40e6cb3b076eaaec9e65f4f3ba5bc
Amira <amira.sheremet@gmail.com>
2015-12-08 20:09:16 +0200

    created new project: web_calendar.Infrastructure and two classes: EmailModel and EmailSender to send emails

---
+++ web_calendar/web_calendar.Infrastructure/Services/EmailModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_calendar.Infrastructure.Services
+{
+    public class EmailModel
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ web_calendar/web_calendar.Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_calendar.Infrastructure.Services
+{
+    public static class EmailSender
+    {
+        private static AppSettingsReader config = null;
+
+        private static AppSettingsReader Config
+        {
+            get
+            {
+                if (config == null)
+                    config = new AppSettingsReader();
+                return config;
+            }
+        }
+
+        public static void SendMail(EmailModel email)
+        {
+            MailMessage mail = new MailMessage();
+            mail.To.Add(email.To == string.Empty ? (string)Config.GetValue("EmailName", typeof(string)) : email.To);
+            mail.Subject = "Invitation";
+            mail.From = new MailAddress(email.From == string.Empty ? 
+                (string)Config.GetValue("EmailName", typeof(string)) : email.From);
+            mail.Body = email.Body;
+            mail.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = (int)Config.GetValue("EmailPort", typeof(int)),
+                UseDefaultCredentials = false,
+                Credentials = new System.Net.NetworkCredential
+                    ((string)Config.GetValue("EmailName", typeof(string)),
+                     (string)Config.GetValue("EmailPassword", typeof(string))),
+                EnableSsl = true
+            };
+
+            smtp.Send(mail);
+        }
+    }
+}
